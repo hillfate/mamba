f08bb152ddaa7da145c163855e69db67a0be4db6
Yanxia Deng <ydeng2033@gmail.com>
2025-05-08 15:25:11 -0400

    update rdzv_id

--- samba/run_train.sh
+++ samba/run_train.sh
@@ -1,7 +1,7 @@
 export MASTER_ADDR=$(hostname)  # Set master node address
 export MASTER_PORT=29500         # Default PyTorch port, change if needed
 
-torchrun --nnodes=1 --nproc_per_node=8 --rdzv_id=bigram_mamba_430m --rdzv_backend=c10d --rdzv_endpoint=${MASTER_ADDR}:${MASTER_PORT} \
+torchrun --nnodes=1 --nproc_per_node=8 --rdzv_id=Mamba_430M --rdzv_backend=c10d --rdzv_endpoint=${MASTER_ADDR}:${MASTER_PORT} \
     pretrain.py \
     --train_data_dir /network/rit/dgx/dgx_yinlab/ydeng/data/slimPajama/tokenized_data \
     --val_data_dir /network/rit/dgx/dgx_yinlab/ydeng/data/slimPajama/tokenized_data # Change to custom train and validation data directory
